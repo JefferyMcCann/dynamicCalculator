c3d1ca443a21a130754d2ba73af3872ef2489d4f
Jeff <126430216+JefferyMcCann@users.noreply.github.com>
2023-03-07 16:53:08 -0800

    Create dynamicCalcV0.cpp

---
+++ v0/dynamicCalcV0.cpp
@@ -0,0 +1,26 @@
+/* this is a dynamic calculator I am working on in c++, I am switching from c to c++ as 
+there are no jobs for c programmers in my area. I am still going to use c for personal 
+projects as I love the language.*/ 
+
+#include <iostream> 
+
+int main() {
+	std::cout << "this is a test \n"; 
+
+	int a;
+
+	int b; 
+
+	std::cin >> a; 
+
+	std::cin >> b; 
+
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	int c = a + b;  
+
+	std::cout << c; 
+} /* this is a very early version of the program with no dynamic elements. I am currently learning
+     as I go on and you will find later version with dynamic elements on my github soon */  
